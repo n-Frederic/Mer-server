e728a6e51c7554f757da2727ce848570f94fff9e
n-Frederic <xuechaozhou001@gmail.com>
2025-10-10 15:09:06 +0800

    update verificationCode

--- src/main/java/com/example/demo/entity/VerificationCode.java
+++ src/main/java/com/example/demo/entity/VerificationCode.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.time.LocalDateTime;
 
