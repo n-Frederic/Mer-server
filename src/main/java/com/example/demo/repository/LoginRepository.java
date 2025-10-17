817a7a16be86128462220948533edc206f4169e8
n-Frederic <xuechaozhou001@gmail.com>
2025-10-17 15:03:29 +0800

    update the valid time of token

--- src/main/java/com/example/demo/repository/LoginRepository.java
+++ src/main/java/com/example/demo/repository/LoginRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 
 public interface LoginRepository extends JpaRepository<Login, Long> {
     Optional<Login> findByToken(String token);
+    Optional<Login> findByUser(User user);
 }
