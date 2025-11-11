f1bb353df0578a0420b127382fcb417192a48f32
n-Frederic <xuechaozhou001@gmail.com>
2025-11-11 18:08:32 +0800

    Update url

--- src/main/java/com/example/demo/controller/TaskProgressController.java
+++ src/main/java/com/example/demo/controller/TaskProgressController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/tasks")
+@RequestMapping("/tasks")
 public class TaskProgressController {
 
     private final TaskProgressService taskProgressService;
