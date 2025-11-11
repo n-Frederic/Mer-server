f8732832a7e7bbb724d8e1c09c1b7f53fb848149
n-Frederic <xuechaozhou001@gmail.com>
2025-11-11 17:47:54 +0800

    update taskUpdate

--- src/main/java/com/example/demo/repository/TaskRepository.java
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -7,7 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
     Page<Task> findByCreatorIdAndStatusContainingAndPriorityContaining(
             Long creatorId, String status, String priority, Pageable pageable);
 
