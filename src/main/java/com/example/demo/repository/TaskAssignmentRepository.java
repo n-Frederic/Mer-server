09c8e3ff2cdb3fc2ebbc9d3c0c1190e33a8f6c32
05hal <1831437770@qq.com>
2025-11-17 21:51:19 +0800

    Update TaskAssignmentRepository.java

--- src/main/java/com/example/demo/repository/TaskAssignmentRepository.java
+++ src/main/java/com/example/demo/repository/TaskAssignmentRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import com.example.demo.dto.AssigneeDTO;
 import com.example.demo.dto.StatusCount;
 import com.example.demo.entity.Task;
 import com.example.demo.entity.TaskAssignment;
@@ -15,18 +16,23 @@ import java.util.Optional;
 
 @Repository
 public interface TaskAssignmentRepository extends JpaRepository<TaskAssignment, Long> {
-    List<TaskAssignment> findByTaskId(Long taskId);
 
-    @Query("""
-        SELECT t.status AS status, COUNT(t) AS count
-        FROM TaskAssignment ta
-        JOIN Task t ON ta.taskId = t.id
-        WHERE ta.assignedAt BETWEEN :start AND :end AND ta.assigneeId = :userId
-        GROUP BY t.status
-    """)
+
+
     List<StatusCount> findWeeklyTaskStatusCount(
             @Param("userId") Long userId,
             @Param("start") LocalDateTime start,
             @Param("end") LocalDateTime end
     );
+    List<TaskAssignment> findByTaskId(Long taskId);
+    @Query(value = """
+            SELECT 
+                u.user_id AS userId,
+                u.name    AS name
+            FROM task_assignment ta
+            JOIN user u ON ta.assignee_id = u.user_id
+            WHERE ta.task_id = :taskId
+            """, nativeQuery = true)
+    List<AssigneeDTO> findAssigneesByTaskId(@Param("taskId") Long taskId);
 }
+
