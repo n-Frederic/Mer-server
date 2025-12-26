e2957f525f1f2c0872ba22c1cf2e0f9f856a0ac2
n-Frederic <xuechaozhou001@gmail.com>
2025-12-26 11:22:37 +0800

    Update

--- src/main/java/com/example/demo/service/CosService.java
+++ src/main/java/com/example/demo/service/CosService.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.io.InputStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
